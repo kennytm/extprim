0b00be2e4403b80ca002ad288c4e4dde28f71bfd
kennytm <kennytm@gmail.com>
2015-01-01 20:27:34 +0800

    License.txt and some misc change.

--- generate_assembly.sh
+++ generate_assembly.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rustc --opt-level 3 --emit asm --crate-type lib -C no-stack-check src/lib.rs
-cat lib.s | c++filt | expand -t 8 | sed 's/\$LT\$/</g;s/\$GT\$/>/g;s/\$C\$/,/g;s/\$x20/ /g' > lib2.s
+cat lib.s | c++filt | expand -t 8 | sed 's/\$LT\$/</g;s/\$GT\$/>/g;s/\$C\$/,/g;s/\$u{20}/ /g' > lib2.s
 mv lib2.s lib.s
