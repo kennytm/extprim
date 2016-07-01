9ef164cc22882cb4aa4ef57a5e0ee50537f5e13e
kennytm <kennytm@gmail.com>
2016-07-01 11:57:17 +0800

    Remove Rust 1.8.0 support. Remove the `cast_unstable.rs` workaround.

--- build.rs
+++ build.rs
@@ -1,6 +1,5 @@
 extern crate rustc_version;
 use rustc_version::{version_meta, Channel};
-use std::{env, fs};
 
 pub fn main() {
     let channel = match version_meta().channel {
@@ -8,9 +7,5 @@ pub fn main() {
         Channel::Beta | Channel::Stable => "stable",
     };
     println!("cargo:rustc-cfg=extprim_channel=\"{}\"", channel);
-
-    let src = format!("src/cast_{}.rs", channel);
-    let target = format!("{}/cast.rs", env::var("OUT_DIR").unwrap());
-    fs::copy(src, target).unwrap();
 }
 
--- src/traits.rs
+++ src/traits.rs
@@ -96,8 +96,6 @@ macro_rules! impl_to_extra_primitive_for_float {
 impl_to_extra_primitive_for_float!(f32, 23, 104, 103);
 impl_to_extra_primitive_for_float!(f64, 52, 75, 74);
 
-include!(concat!(env!("OUT_DIR"), "/cast.rs"));
-
 #[cfg(test)]
 mod float_to_128_tests {
     use u128::u128;
@@ -222,6 +220,78 @@ mod float_to_128_tests {
         assert_eq!((f32::MAX).to_i128(), Some(i128::zero()));
         assert_eq!((f32::MIN_POSITIVE).to_i128(), Some(i128::zero()));
         assert_eq!((f32::INFINITY).to_i128(), Some(i128::zero()));
+    }
+}
+
+#[cfg(extprim_channel="unstable")]
+impl<T: ToPrimitive> ToExtraPrimitive for T {
+    default fn to_u128(&self) -> Option<u128> {
+        self.to_u64().map(u128::new)
+    }
+
+    default fn to_i128(&self) -> Option<i128> {
+        match self.to_u64() {
+            Some(v) => Some(i128(u128::new(v))),
+            None => self.to_i64().map(i128::new),
+        }
+    }
+}
+
+impl NumCast for u128 {
+    #[cfg(extprim_channel="unstable")]
+    fn from<T: ToPrimitive>(n: T) -> Option<u128> {
+        n.to_u128()
+    }
+
+    #[cfg(extprim_channel="stable")]
+    fn from<T: ToPrimitive>(_: T) -> Option<u128> {
+        panic!("cannot use this outside of nightly rust yet");
+    }
+}
+
+impl NumCast for i128 {
+    #[cfg(extprim_channel="unstable")]
+    fn from<T: ToPrimitive>(n: T) -> Option<i128> {
+        n.to_i128()
+    }
+
+    #[cfg(extprim_channel="stable")]
+    fn from<T: ToPrimitive>(_: T) -> Option<i128> {
+        panic!("cannot use this outside of nightly rust yet");
+    }
+}
+
+#[cfg(all(test, extprim_channel="unstable"))]
+mod num_cast_tests {
+    use std::u64;
+    use num_traits::{NumCast, One, Bounded};
+    use u128::u128;
+    use i128::i128;
+
+    #[test]
+    fn test_num_cast_for_u128() {
+        assert_eq!(None::<u64>, NumCast::from(-1i8)); // sanity check.
+        assert_eq!(None::<u128>, NumCast::from(-1i8));
+        assert_eq!(Some(u128::one()), NumCast::from(1i8));
+        assert_eq!(Some(u128::new(u64::MAX)), NumCast::from(u64::MAX));
+        assert_eq!(Some(u128::max_value()), NumCast::from(u128::max_value()));
+
+        assert_eq!(Some(u128::one()), NumCast::from(i128::new(1)));
+        assert_eq!(None::<u128>, NumCast::from(i128::new(-1)));
+    }
+
+    #[test]
+    fn test_num_cast_for_i128() {
+        assert_eq!(None::<i64>, NumCast::from(0x8000_0000_0000_0000u64)); // sanity check.
+        assert_eq!(None::<i128>, NumCast::from(u128::max_value()));
+        assert_eq!(Some(i128::one()), NumCast::from(1i8));
+        assert_eq!(Some(-i128::one()), NumCast::from(-1i8));
+        assert_eq!(Some(i128::from_parts(0, 0x8000_0000_0000_0000)), NumCast::from(0x8000_0000_0000_0000u64));
+        assert_eq!(Some(i128::max_value()), NumCast::from(i128::max_value()));
+        assert_eq!(Some(i128::min_value()), NumCast::from(i128::min_value()));
+
+        assert_eq!(Some(i128::one()), NumCast::from(i128::new(1)));
+        assert_eq!(None::<i128>, NumCast::from(u128::from_parts(0x8000_0000_0000_0000, 0)));
     }
 }
 
