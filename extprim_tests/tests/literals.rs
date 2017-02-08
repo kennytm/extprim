60cbbeb336f8fa36314f46f0826489d246582229
kennytm <kennytm@gmail.com>
2017-02-08 17:36:21 +0800

    The test case needs to have rustc_i128 removed as well.
    Restore the IMIN test as rust-lang/rust#38987 is fixed.

--- extprim_tests/tests/literals.rs
+++ extprim_tests/tests/literals.rs
@@ -1,4 +1,3 @@
 #![cfg_attr(extprim_tests_channel="unstable", feature(const_fn, i128_type))]
-#[cfg(extprim_tests_channel="unstable")] extern crate rustc_i128;
 
 include!(concat!(env!("OUT_DIR"), "/literals.rs"));
